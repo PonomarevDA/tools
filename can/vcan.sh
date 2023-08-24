ff595c5a165b3a22ab3c2364f8b04a3b824a8651
PonomarevDA <ponomarevda96@gmail.com>
2023-08-25 00:25:48 +0300

    add cyphal init ci

--- can/vcan.sh
+++ can/vcan.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 script_name=`basename "$0"`
 
 print_help() {
-    echo "usage: $script_name [-h] port"
+    echo "usage: $script_name [-h] <port>"
+    echo "example: $script_name slcan0"
 }
 
 if [ "$1" = "--help" ]; then
     print_help
-    exit 0
+    [[ "${BASH_SOURCE[0]}" -ef "$0" ]] && exit 0 || return
 elif [ $# -ne 1 ]; then
     print_help
     echo "$script_name: error: the following arguments are required: port"
-    exit 0
+    [[ "${BASH_SOURCE[0]}" -ef "$0" ]] && exit 0 || return
 fi
+
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 modprobe vcan
 ip link add dev $1 type vcan
