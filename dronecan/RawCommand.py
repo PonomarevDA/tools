9255285a736482d25cace3ca68ee533c96193bfa
olegogogo <oleg.ostapovich@bk.ru>
2023-09-05 21:11:31 +0300

    pylint fixes

--- dronecan/RawCommand.py
+++ dronecan/RawCommand.py
@@ -1,3 +1,8 @@
+"""
+This script sends RawMessage to node to test servo motor life.
+To run this script you must first create an slcan from the serial port with the specified name.
+For example, the name 'can0' has been used.
+"""
 import time
 from argparse import ArgumentParser
 import dronecan
@@ -6,23 +11,47 @@ from dronecan import uavcan
 # get command line arguments
 parser = ArgumentParser(description='Sends RawCommand')
 parser.add_argument("--port", default='can0', type=str, help="CAN interface name")
-parser.add_argument("--time", default=0, type=str,
+parser.add_argument("--duration", default='0', type=str,
                     help="time till script shutdown, 0 means that it won't stop")
+parser.add_argument("--timeout", default='2', type=str,
+                    help="time duration between max and min position of servomotor")
 
 args = parser.parse_args()
 
 node = dronecan.make_node(args.port, node_id=105, bitrate=1000000)
 
-while True:
+start_time = time.time()
+i = [8191, 0, 0, 0, 0, 0, 0, 0]  # First message
 
-    for i in [[8191, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]:
-        print('new spin started')
-        # print(i)
-        def do_publish():
-            msg = uavcan.equipment.esc.RawCommand(cmd=i)
-            node.broadcast(msg)
 
-            print('publishing')
+def do_publish():
+    """
+    Function to publish RawCommand. 'i' is a variable,
+    that switches globally at the end of this script
+    """
+    msg = uavcan.equipment.esc.RawCommand(cmd=i)
+    node.broadcast(msg)
 
-        node.periodic(1/10, do_publish)
-        node.spin(timeout=2)
+
+node.periodic(1 / 15, do_publish)
+
+
+def switch_data():
+    """
+    Switches messages and spin node for desired time
+    :param i: data of message that affects on do_publish function
+    :return:
+    """
+    global i
+    i = [8191, 0, 0, 0, 0, 0, 0, 0]
+    node.spin(timeout=int(args.timeout))
+    i = [0, 0, 0, 0, 0, 0, 0, 0]
+    node.spin(timeout=int(args.timeout))
+
+
+if args.duration == '0':  # If duration is '0' then script never ends
+    while True:
+        switch_data()
+else:
+    while start_time + int(args.duration) > time.time():
+        switch_data()
