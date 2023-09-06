457890c41a90ceb5363a2ef4ee368340a2a23bd7
olegogogo <oleg.ostapovich@bk.ru>
2023-09-06 15:35:21 +0300

    logging added

--- dronecan/servo_tester.py
+++ dronecan/servo_tester.py
@@ -4,6 +4,7 @@ This script sends RawMessage to node to test servo motor life.
 To run this script you must first create an slcan from the serial port with the specified name.
 For example, the name 'can0' has been used.
 """
+import datetime
 import time
 from argparse import ArgumentParser
 import dronecan
@@ -14,7 +15,7 @@ parser = ArgumentParser(description='Sends RawCommand')
 parser.add_argument("--port", default='can0', type=str, help="CAN interface name")
 parser.add_argument("--duration", default='0', type=str,
                     help="time till script shutdown, 0 means that it won't stop")
-parser.add_argument("--timeout", default='2', type=str,
+parser.add_argument("--timeout", default='0.5', type=str,
                     help="time duration between max and min position of servomotor")
 
 args = parser.parse_args()
@@ -22,7 +23,7 @@ args = parser.parse_args()
 node = dronecan.make_node(args.port, node_id=105, bitrate=1000000)
 
 start_time = time.time()
-i = [8191, 0, 0, 0, 0, 0, 0, 0]  # First message
+i = [1614, 0, 0, 0, 0, 0, 0, 0]  # First message
 
 
 def do_publish():
@@ -34,7 +35,18 @@ def do_publish():
     node.broadcast(msg)
 
 
+message = ''
 node.periodic(1 / 15, do_publish)
+MOVE_COUNTER = 0
+
+
+def handle_node_info(msg):
+    global message
+    message = (f"Uptime: {datetime.timedelta(seconds=int(str(dict(msg.transfer.payload._fields)['uptime_sec'])))}, " +
+               f"Health: {dict(msg.transfer.payload._fields)['health']}")
+
+
+node.add_handler(uavcan.protocol.NodeStatus, handle_node_info)
 
 
 def switch_data():
@@ -44,11 +56,14 @@ def switch_data():
     :return:
     """
     try:
-        global i
-        i = [8191, 0, 0, 0, 0, 0, 0, 0]
-        node.spin(timeout=int(args.timeout))
-        i = [0, 0, 0, 0, 0, 0, 0, 0]
-        node.spin(timeout=int(args.timeout))
+        global i, MOVE_COUNTER, message
+        i = [7000, 0, 0, 0, 0, 0, 0, 0]
+        node.spin(timeout=float(args.timeout))
+        i = [100, 0, 0, 0, 0, 0, 0, 0]
+        node.spin(timeout=float(args.timeout))
+        MOVE_COUNTER += 2
+        file = open('result.txt', 'w')
+        print(f'Movements: {MOVE_COUNTER}, ', message, file=file)
     except dronecan.driver.common.TxQueueFullError:
         print('Exception dronecan.driver.common.TxQueueFullError catched')
     except dronecan.transport.TransferError:
