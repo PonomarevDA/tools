851b93825a27f3ceeca90b1408a9d318827f492e
PonomarevDA <ponomarevda96@gmail.com>
2023-09-10 14:48:59 +0300

    improve registers checker

--- cyphal/specification_checker.py
+++ cyphal/specification_checker.py
@@ -16,26 +16,27 @@ import uavcan.register
 import uavcan.register.List_1_0
 
 
-async def main(dest_node_id):
-    software_version = uavcan.node.Version_1_0(major=1, minor=0)
-    node_info = uavcan.node.GetInfo_1_0.Response(
-        software_version,
-        name="co.raccoonlab.spec_checker"
-    )
-    cyphal_node = pycyphal.application.make_node(node_info)
-    cyphal_node.heartbeat_publisher.mode = uavcan.node.Mode_1_0.OPERATIONAL
-    cyphal_node.start()
-
-    await NodeNameChecker(cyphal_node, dest_node_id).run()
-    await HearbeatFrequencyChecker(cyphal_node, dest_node_id).run()
-    await RegistersNameChecker(cyphal_node, dest_node_id).run()
-    await PortRegistersTypeChecker(cyphal_node, dest_node_id).run()
-    await DefaultRegistersExistanceChecker(cyphal_node, dest_node_id).run()
-
-
 def np_array_to_string(np_array):
     return "".join([chr(item) for item in np_array])
 
+async def request_regiter_names(cyphal_node, dest_node_id, max_register_amount=256):
+    register_names = []
+    list_request = uavcan.register.List_1_0.Request()
+    list_client = cyphal_node.make_client(uavcan.register.List_1_0, dest_node_id)
+    for _ in range(max_register_amount):
+        list_response = await list_client.call(list_request)
+        if list_response is None:
+            return None
+
+        register_name = np_array_to_string(list_response[0].name.name)
+        if len(register_name) == 0:
+            break
+
+        register_names.append(register_name)
+        await asyncio.sleep(0.001)
+        list_request.index += 1
+
+    return register_names
 
 class NodeNameChecker:
     def __init__(self, cyphal_node, dest_node_id):
@@ -96,26 +97,8 @@ class HearbeatFrequencyChecker:
         periods = [timestamps[i] - timestamps[i - 1] for i in range(1, len(timestamps))]
         return min(periods), max(periods)
 
-async def request_regiter_names(cyphal_node, dest_node_id, max_register_amount=256):
-    register_names = []
-    list_request = uavcan.register.List_1_0.Request()
-    list_client = cyphal_node.make_client(uavcan.register.List_1_0, dest_node_id)
-    for _ in range(max_register_amount):
-        list_response = await list_client.call(list_request)
-        if list_response is None:
-            return None
-
-        register_name = np_array_to_string(list_response[0].name.name)
-        if len(register_name) == 0:
-            break
-
-        register_names.append(register_name)
-        await asyncio.sleep(0.001)
-        list_request.index += 1
-
-    return register_names
-
 class RegistersNameChecker:
+    """https://github.com/OpenCyphal/public_regulated_data_types/blob/master/uavcan/register/384.Access.1.0.dsdl"""
     def __init__(self, cyphal_node, dest_node_id):
         self._node = cyphal_node
         self._dest_node_id = dest_node_id
@@ -133,33 +116,56 @@ class RegistersNameChecker:
         return re.match(pattern, register_name) is not None
 
 class PortRegistersTypeChecker:
+    """https://github.com/OpenCyphal/public_regulated_data_types/blob/master/uavcan/register/384.Access.1.0.dsdl"""
     def __init__(self, cyphal_node, dest_node_id):
         self._node = cyphal_node
         self._dest_node_id = dest_node_id
 
     async def run(self):
-        is_test_successfull = True
         register_names = await request_regiter_names(self._node, self._dest_node_id)
 
+        ports = {}
         access_request = uavcan.register.Access_1_0.Request()
         access_client = self._node.make_client(uavcan.register.Access_1_0, self._dest_node_id)
         for register_name in register_names:
-            if not register_name.startswith("uavcan.") or not register_name.endswith(".id"):
+            if self.get_port_type_by_register_name(register_name) is None:
                 continue
 
             access_request.name.name = register_name
             access_response = await access_client.call(access_request)
-            if access_response is None:
-                is_test_successfull = False
-                continue
 
-            if access_response[0].value.natural16 is None:
-                is_test_successfull = False
-                print(f"{register_name} should have natural16 type")
+            ports[register_name] = None if access_response is None else access_response[0].value
 
-        return is_test_successfull
+        is_successfull = False
+        for port_name, value in ports.items():
+            if value is None:
+                if not is_successfull:
+                    print(f"Violation of: {PortRegistersTypeChecker.__doc__}\nDetails:")
+                    is_successfull = False
+                print(f"- {port_name} failed")
+            elif value.natural16 is None:
+                if not is_successfull:
+                    print(f"Violation of: {PortRegistersTypeChecker.__doc__}\nDetails:")
+                    is_successfull = False
+                print(f"- {port_name} should have natural16 type, but it is {value}.")
+
+    @staticmethod
+    def get_port_type_by_register_name(register_name):
+        port_type = None
+
+        if register_name.startswith("uavcan.pub") and register_name.endswith(".id"):
+            port_type = 'pub'
+        elif register_name.startswith("uavcan.sub") and register_name.endswith(".id"):
+            port_type = 'sub'
+        elif register_name.startswith("uavcan.cln") and register_name.endswith(".id"):
+            port_type = 'cln'
+        elif register_name.startswith("uavcan.srv") and register_name.endswith(".id"):
+            port_type = 'srv'
+
+        return port_type
 
 class DefaultRegistersExistanceChecker:
+    """https://github.com/OpenCyphal/public_regulated_data_types/blob/master/uavcan/register/384.Access.1.0.dsdl"""
     def __init__(self, cyphal_node, dest_node_id):
         self._node = cyphal_node
         self._dest_node_id = dest_node_id
@@ -185,6 +191,23 @@ class DefaultRegistersExistanceChecker:
                 print(f"{register_name} is not exist")
 
         return is_test_successfull
+
+
+async def main(dest_node_id):
+    software_version = uavcan.node.Version_1_0(major=1, minor=0)
+    node_info = uavcan.node.GetInfo_1_0.Response(
+        software_version,
+        name="co.raccoonlab.spec_checker"
+    )
+    cyphal_node = pycyphal.application.make_node(node_info)
+    cyphal_node.heartbeat_publisher.mode = uavcan.node.Mode_1_0.OPERATIONAL
+    cyphal_node.start()
+
+    await NodeNameChecker(cyphal_node, dest_node_id).run()
+    await HearbeatFrequencyChecker(cyphal_node, dest_node_id).run()
+    await RegistersNameChecker(cyphal_node, dest_node_id).run()
+    await PortRegistersTypeChecker(cyphal_node, dest_node_id).run()
+    await DefaultRegistersExistanceChecker(cyphal_node, dest_node_id).run()
 
 if __name__ == "__main__":
     asyncio.run(main(dest_node_id=50))
