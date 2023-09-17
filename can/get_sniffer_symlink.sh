6ddf586d38b562370f63cb83cf43e4efb05688df
PonomarevDA <ponomarevda96@gmail.com>
2023-09-17 20:02:53 +0300

    add zubax babel support

--- can/get_sniffer_symlink.sh
+++ can/get_sniffer_symlink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Brief: Get RaccoonLab sniffer symlink
+# Brief: Get a sniffer symlink
 # Usage: source get_sniffer_symlinks.sh
 # Output environment variables:
 # - DEV_PATH
@@ -14,24 +14,37 @@ if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
     exit 1
 fi
 
-EXPECTED_VID=0483
-EXPECTED_PID=374b
 EXPECTED_DEV_PATH="/dev/ttyACM*"
 EXPECTED_SYMLINK_PATH="/dev/serial/by-id/"
 
+# Known sniffers:
+RACCOON_LAB_VID=0483
+RACCOON_LAB_PID=374b
+ZUBAX_BABEL_VID=1d50
+ZUBAX_BABEL_PID=60c7
+
+
 for dev_path in $EXPECTED_DEV_PATH; do
     [ -e "$dev_path" ] || continue
-    check_vid_and_pid=$(udevadm info $dev_path |
-                        grep -E "(ID_MODEL_ID=$EXPECTED_PID|ID_VENDOR_ID=$EXPECTED_VID)" -wc || true)
-    if [ "$check_vid_and_pid" == 2 ]; then
+    is_rl_sniffer=$(udevadm info $dev_path | grep -E "(ID_MODEL_ID=$RACCOON_LAB_PID|ID_VENDOR_ID=$RACCOON_LAB_VID)" -wc || true)
+    if [ "$is_rl_sniffer" == 2 ]; then
         DEV_PATH=$dev_path
         DEV_PATH_SYMLINK=$(find -L $EXPECTED_SYMLINK_PATH -samefile $DEV_PATH)
+        echo "RaccoonLab sniffer has been detected."
+        break
+    fi
+
+    is_zubax_sniffer=$(udevadm info $dev_path | grep -E "(ID_MODEL_ID=$ZUBAX_BABEL_PID|ID_VENDOR_ID=$ZUBAX_BABEL_VID)" -wc || true)
+    if [ "$is_zubax_sniffer" == 2 ]; then
+        DEV_PATH=$dev_path
+        DEV_PATH_SYMLINK=$(find -L $EXPECTED_SYMLINK_PATH -samefile $DEV_PATH)
+        echo "$SCRIPT_NAME: Zubax Babel-Babel has been detected."
         break
     fi
 done
 
 if [ -z "$DEV_PATH_SYMLINK" ]; then
-    printf "$RED$SCRIPT_NAME Can't find RaccoonLab sniffer!$NC\n"
+    printf "$RED$SCRIPT_NAME Can't find a sniffer!$NC\n"
     # Note: We must use return in a sourced script, otherwise it must be exit!
     [[ "${BASH_SOURCE[0]}" -ef "$0" ]] && exit 0 || return
 fi
