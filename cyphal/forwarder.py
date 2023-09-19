6e2d50edd17cffa26081b21b354ce20d46f6e574
PonomarevDA <ponomarevda96@gmail.com>
2023-09-19 22:48:07 +0300

    refactor forwarder

--- cyphal/forwarder.py
+++ cyphal/forwarder.py
@@ -1,93 +1,43 @@
 #!/usr/bin/env python3
-import time
 import asyncio
 import sys
 import pathlib
 import subprocess
+from fragments import CyphalFragmentSub, CyphalFragmentPub
 
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "build/nunavut_out"))
 import pycyphal.application
 import uavcan.node
 import uavcan.node.GetInfo_1_0 as GetInfo_1_0
-import uavcan.metatransport.serial.Fragment_0_2 as Fragment_0_2
 
+# RaccoonLab node corresponded registers
 UBX_RX_REG = "uavcan.sub.gps.ubx_rx.id"
 UBX_TX_REG = "uavcan.pub.gps.ubx_tx.id"
 
-
-class UbxTxFragmentSub:
-    def __init__(self, node, port_id, sub_verbose) -> None:
-        node.registry["uavcan.sub.ubx_tx.id"] = port_id
-        self._sp_sub = node.make_subscriber(Fragment_0_2, "ubx_tx")
-        self._sp_sub.receive_in_background(self._sub_cb)
-        self._rx_counter = 0
-        self._verbose = sub_verbose
-
-    async def _sub_cb(self, msg, _):
-        if self._verbose:
-            await self._print(msg)
-
-    async def _print(self, msg, chunk_size=22):
-        total_size = len(msg.data.tobytes())
-        for first_index in range(0, total_size, chunk_size):
-            last_index = first_index + min(total_size - first_index, chunk_size)
-            length = last_index - first_index
-            print(f"RX[{length : < 3}] ", end ="")
-            for idx in range(first_index, last_index):
-                if msg.data[idx] == 181:
-                    byte_str = f"\033[93m{msg.data[idx]}\033[0m"
-                elif msg.data[idx] == 98:
-                    byte_str = f"\033[95m{msg.data[idx]}\033[0m"
-                else:
-                    byte_str = msg.data[idx]
-
-                print(byte_str, end =" ")
-            print("")
-            self._rx_counter += length
-
-class TimeSub:
-    def __init__(self, node, port_id) -> None:
-        node.registry["uavcan.sub.time_utc.id"] = port_id
-        self._sp_sub = node.make_subscriber(uavcan.time.SynchronizedTimestamp_1_0, "time_utc")
-        self._sp_sub.receive_in_background(self._sub_cb)
-
-    async def _sub_cb(self, msg, _):
-        pass
-
-class UbxRxFragmentPub:
-    def __init__(self, node, port_id) -> None:
-        node.registry["uavcan.pub.ubx_rx.id"] = port_id
-        self._msg = Fragment_0_2()
-        self._pub = node.make_publisher(Fragment_0_2, "ubx_rx")
-
-    async def publish(self, text):
-        self._msg.data = text
-        print(f"TX[{len(text) : < 3}] {text}")
-        await self._pub.publish(self._msg)
 
 class SerialForwarder:
     def __init__(self, node_id):
         self._ubx_tx_port_id = get_node_register_value(node_id, UBX_TX_REG)
         self._ubx_rx_port_id = get_node_register_value(node_id, UBX_RX_REG)
 
-        print(f" --------------------                                        --------------")
-        print(f" |              PUB | >> {UBX_RX_REG} ({self._ubx_rx_port_id :<5}) >> | RXI        |")
-        print(f" | forwarder.py     |                                        |      UBLOX |")
-        print(f" |              SUB | << {UBX_TX_REG} ({self._ubx_tx_port_id :<5}) << | TXO        |")
-        print(f" --------------------                                        --------------")
+        print(f" --------------------    -----------------------------    --------------")
+        print(f" | forwarder.py     |    | RL GPS_MAG_BARO NODE      |    | UBLOX GNSS |")
+        print(f" |              PUB | >> | gps.ubx_rx={self._ubx_rx_port_id :<5}  UART TX | >> | UART RX    |")
+        print(f" |              SUB | << | gps.ubx_tx={self._ubx_tx_port_id :<5}  UART RX | << | UART TX    |")
+        print(f" --------------------    -----------------------------    --------------")
         print("")
 
         if self._ubx_tx_port_id == 65535 or self._ubx_rx_port_id == 65535:
             print("Error: required registers are not configured!")
             exit(-1)
 
-    def run(self, commands, send_loop=False, recv_loop=False, sub_verbose=False):
+    def run(self, commands, send_loop=False, recv_loop=False, verbose=False):
         try:
-            asyncio.run(self._main(commands, send_loop, recv_loop, sub_verbose))
+            asyncio.run(self._main(commands, send_loop, recv_loop, verbose))
         except KeyboardInterrupt:
             pass
 
-    async def _main(self, commands, send_loop, recv_loop, sub_verbose):
+    async def _main(self, commands, send_loop, recv_loop, verbose):
         node_info = GetInfo_1_0.Response(
             software_version=uavcan.node.Version_1_0(major=1, minor=0),
             name="ubx"
@@ -98,9 +48,8 @@ class SerialForwarder:
     
         self._node.start()
 
-        self.time_utc_sub = TimeSub(self._node, get_node_register_value(50, "uavcan.pub.gps.time_utc.id"))
-        self.ubx_rx_serial_pub = UbxRxFragmentPub(self._node, self._ubx_rx_port_id)
-        self.ubx_tx_serial_sub = UbxTxFragmentSub(self._node, self._ubx_tx_port_id, sub_verbose)
+        self.ubx_rx_serial_pub = CyphalFragmentPub(self._node, self._ubx_rx_port_id, verbose)
+        self.ubx_tx_serial_sub = CyphalFragmentSub(self._node, self._ubx_tx_port_id, verbose)
         self._node.start()
 
         command_index = 0
@@ -120,6 +69,11 @@ def get_node_register_value(node_id, register_name):
     return int(output)
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Cyphal specification checker')
+    parser.add_argument("--verbose", default=False, action='store_true', help="Verbose mode")
+    args = parser.parse_args()
+
     commands = [
         "ubx reset",
         "ubx set baudrate",
@@ -128,4 +82,4 @@ if __name__ == "__main__":
         "ubx save config"
     ]
 
-    SerialForwarder(node_id=50).run(commands, True, True, sub_verbose=False)
+    SerialForwarder(node_id=50).run(commands, True, True, verbose=args.verbose)
---
+++ cyphal/fragments.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import sys
+import pathlib
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "build/nunavut_out"))
+import pycyphal.application
+import uavcan.metatransport.serial.Fragment_0_2 as Fragment_0_2
+
+class CyphalFragmentSub:
+    def __init__(self, node : pycyphal.application._node_factory.SimpleNode, port_id : int, verbose : bool) -> None:
+        self._sp_sub = node.make_subscriber(Fragment_0_2, port_id)
+        self._sp_sub.receive_in_background(self._sub_cb)
+        self._rx_counter = 0
+        self._verbose = verbose
+
+    async def _sub_cb(self, msg, _):
+        if self._verbose:
+            await self._print(msg)
+
+    async def _print(self, msg, chunk_size=22):
+        total_size = len(msg.data.tobytes())
+        for first_index in range(0, total_size, chunk_size):
+            last_index = first_index + min(total_size - first_index, chunk_size)
+            length = last_index - first_index
+            print(f"RX[{length : < 3}] ", end ="")
+            for idx in range(first_index, last_index):
+                if msg.data[idx] == 181:
+                    byte_str = f"\033[93m{msg.data[idx]}\033[0m"
+                elif msg.data[idx] == 98:
+                    byte_str = f"\033[95m{msg.data[idx]}\033[0m"
+                else:
+                    byte_str = msg.data[idx]
+
+                print(byte_str, end =" ")
+            print("")
+            self._rx_counter += length
+
+class CyphalFragmentPub:
+    def __init__(self, node : pycyphal.application._node_factory.SimpleNode, port_id : int, verbose : bool) -> None:
+        self._msg = Fragment_0_2()
+        self._pub = node.make_publisher(Fragment_0_2, port_id)
+        self._verbose = verbose
+
+    async def publish(self, text):
+        self._msg.data = text
+        if self._verbose:
+            print(f"TX[{len(text) : < 3}] {text}")
+        await self._pub.publish(self._msg)
