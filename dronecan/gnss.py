3303475951e2509aff7946d6e26c90628070070e
PonomarevDA <ponomarevda96@gmail.com>
2023-09-20 08:39:55 +0300

    add setup script for dronecan gnss

--- dronecan/gnss.py
+++ dronecan/gnss.py
@@ -1,20 +1,64 @@
 #!/usr/bin/env python3
+import os
+from datetime import date
+import logging
+
 import dronecan
 from dronecan import uavcan
 
 
 class DronecanGpsMagBaroChecker:
-    def __init__(self, dronecan_node):
-        self._dronecan_node = dronecan_node
-        self._dronecan_node.add_handler(uavcan.equipment.gnss.Fix2, self._callback)
+    def __init__(self, node):
+        self._node = node
+
+        node.add_handler(uavcan.equipment.gnss.Fix2, self._gps_callback)
         self._sats_used = None
-        self._required_sats = 8
+        self._required_sats = 6
+
+        node.add_handler(uavcan.equipment.ahrs.MagneticFieldStrength, self._mag_callback)
+        self._magnetic_field_ga = None
+
+        node.add_handler(uavcan.equipment.air_data.StaticPressure, self._static_pressure_callback)
+        self._static_pressure = None
+
+        node.add_handler(uavcan.equipment.air_data.StaticTemperature, self._static_temperature_callback)
+        self._static_temperature = None
+
+        req = uavcan.protocol.GetNodeInfo.Request()
+        node.request(req, 70, self._node_info_callback)
+        self.software_version = None
+        self.hardware_version = None
 
     def is_valid(self):
+        return self.is_gps_valid() and self.is_mag_valid() and self.is_baro_valid()
+
+    def is_gps_valid(self):
         return self._sats_used is not None and self._sats_used >= self._required_sats
+
+    def is_mag_valid(self):
+        return self._magnetic_field_ga is not None
+
+    def is_baro_valid(self):
+        return self._static_pressure is not None and self._static_temperature is not None
 
     def get_sats_used(self):
         return self._sats_used
+
+    def create_report(self, filename):
+        current_date = date.today().strftime("%B %d, %Y")
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write("\nRL GNSS v2.5\n")
+            file.write(f"{current_date}\n")
+            file.write(f"{self.software_version}\n")
+            file.write(f"{self.hardware_version}\n")
+
+            file.write(f"GNSS: {self.is_gps_valid()}\n")
+            file.write(f"Mag: {self.is_mag_valid()}\n")
+            file.write(f"Baro: {self.is_baro_valid()}\n")
 
     def __str__(self) -> str:
         if self._sats_used is None:
@@ -27,9 +71,44 @@ class DronecanGpsMagBaroChecker:
             description = f"Setelites: {self._sats_used}. Goood!"
         return description
 
-    def _callback(self, msg):
-        payload = msg.transfer.payload
-        self._sats_used = payload.sats_used
+    def _node_info_callback(self, msg):
+        major = msg.transfer.payload.software_version.major
+        minor = msg.transfer.payload.software_version.minor
+        vcs_commit = f"{msg.transfer.payload.software_version.vcs_commit:x}"
+
+        unique_id = ""
+        for first_idx in range(0, 12, 4):
+            for value in msg.transfer.payload.hardware_version.unique_id[first_idx : first_idx+4]:
+                unique_id += f"{int(value.value):x} "
+            unique_id += "\n"
+
+        self.software_version = f"SW: v{major}.{minor}_{vcs_commit}"
+        self.hardware_version = f"UID:\n{unique_id}"
+
+    def _gps_callback(self, msg):
+        self._sats_used = msg.transfer.payload.sats_used
+
+    def _mag_callback(self, msg):
+        self._magnetic_field_ga = msg.transfer.payload.magnetic_field_ga
+
+    def _static_pressure_callback(self, msg):
+        self._static_pressure = msg.transfer.payload.static_pressure
+
+    def _static_temperature_callback(self, msg):
+        self._static_temperature = msg.transfer.payload.static_temperature
+
+
+def test_dronecan_gps_mag_baro(port, filename, timeout=180):
+    dronecan_node = dronecan.make_node(port, node_id=100, bitrate=1000000)
+    checker = DronecanGpsMagBaroChecker(dronecan_node)
+    dronecan_node.spin(0.5)
+    for elapsed_time in range(0, timeout, 5):
+        logging.warning("%s/%s: %s", elapsed_time, timeout, checker)
+        if checker.is_gps_valid():
+            break
+        dronecan_node.spin(5)
+
+    checker.create_report(filename)
 
 
 if __name__ == "__main__":
@@ -39,12 +118,4 @@ if __name__ == "__main__":
     parser.add_argument("--timeout", default=10, type=int, help="Waiting for satelites duration. Default 90 seconds")
     args = parser.parse_args()
 
-    dronecan_node = dronecan.make_node(args.port, node_id=100, bitrate=1000000)
-
-    checker = DronecanGpsMagBaroChecker(dronecan_node)
-    dronecan_node.spin(0.5)
-    for elapsed_time in range(0, args.timeout, 5):
-        print(f"{elapsed_time}/{args.timeout}: {checker}")
-        if checker.is_valid():
-            break
-        dronecan_node.spin(5)
+    test_dronecan_gps_mag_baro(args.port, "report.txt", args.timeout)
--- nodes/gnss/setup.py
+++ nodes/gnss/setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 import os
-import wget
+import sys
 import subprocess
+import logging
 from pathlib import Path
+import wget
 
+repo_dir = Path(__file__).resolve().parent.parent.parent
+sys.path.insert(0, str(repo_dir / "cyphal"))
+sys.path.insert(0, str(repo_dir / "dronecan"))
+
+from gnss import test_dronecan_gps_mag_baro
+
+RELEASE_URL = "https://github.com/RaccoonlabDev/docs/releases"
 URLS = {
-    "gnss_v3_cyphal_v1.5.2" : "https://github.com/RaccoonlabDev/docs/releases/download/v1.5.2/gnss_v3_cyphal_v1.5.2_c900ac3.bin",
-    "gnss_v2.5_dronecan_v0.12.1" : "https://github.com/RaccoonlabDev/docs/releases/download/v0.12.2/gnss_v2.5_dronecan_v0.12.1_e0ba916.bin"
+    "gnss_v3_cyphal" : f"{RELEASE_URL}/download/v1.5.2/gnss_v3_cyphal_v1.5.2_c900ac3.bin",
+    "gnss_v2.5_dronecan" : f"{RELEASE_URL}/download/v0.12.2/gnss_v2.5_dronecan_v0.12.1_e0ba916.bin"
 }
 BINARY_FOLDER = 'binaries'
 BINARY_PATH = 'binaries/latest.bin'
@@ -24,19 +33,52 @@ def upload_firmware(binary_path):
 
 def download_firmware_and_upload_to_target(protocol):
     if protocol == 'cyphal':
-        firmware = "gnss_v3_cyphal_v1.5.2"
+        firmware = "gnss_v3_cyphal"
     elif protocol == 'dronecan':
-        firmware = "gnss_v2.5_dronecan_v0.12.1"
+        firmware = "gnss_v2.5_dronecan"
     else:
-        print("Protocol should be specified.")
+        print("Unknown specified protocol.")
         return
 
     download_firmware(URLS[firmware])
     upload_firmware(BINARY_PATH)
+    print("")
+
+def print_report(filename, printer_name="Xerox_Xprinter_XP-365B"):
+    if not os.path.isfile(filename):
+        logging.error("File %s is not exist", filename)
+        return
+
+    res = subprocess.check_output(["lpstat", "-p", printer_name]).decode()
+
+    if printer_name not in res:
+        logging.error("Printer %s is not configured.", printer_name)
+        return
+
+    if 'Unplugged or turned off' in res:
+        logging.error('Printer is unplugged or turned off')
+        return
+
+    proc = subprocess.Popen(f"lp -d {printer_name} {filename}".split(), stdout=subprocess.PIPE)
+    proc.communicate()
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
-    parser = ArgumentParser(description='Node setup: firmware uploader and configurator')
-    parser.add_argument("--protocol", type=str, help="[cyphal, dronecan]")
+    parser = ArgumentParser(description='Node setup: '
+                                        '1. download latest firmware and upload it to the target '
+                                        '2. provide tests '
+                                        '3. print result')
+    parser.add_argument("--protocol", type=str, required=True, help="[cyphal, dronecan]")
+    parser.add_argument("--port", default='slcan0', type=str, help="[slcan0, ...]")
+    parser.add_argument("--printer", default=False, action='store_true', help="Print report")
     args = parser.parse_args()
+
+    REPORT_PATH = "report.txt"
+
     download_firmware_and_upload_to_target(args.protocol)
+
+    if args.protocol == 'dronecan':
+        test_dronecan_gps_mag_baro(args.port, REPORT_PATH)
+
+    if args.printer:
+        print_report(filename=REPORT_PATH)
