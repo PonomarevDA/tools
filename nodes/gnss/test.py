00d60151dee570f0edcbe5c6cb7a822ada1e01c6
PonomarevDA <ponomarevda96@gmail.com>
2023-10-06 00:23:48 +0300

    add cyphal gnss v2.5 firmware

--- nodes/gnss/test.py
+++ nodes/gnss/test.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
                                         '2. Provide tests '
                                         '3. Print result')
     parser.add_argument("--protocol", type=str, required=True, help="[cyphal, dronecan]")
+    parser.add_argument("--hw-version", type=str, required=True, help="[v2, v3]")
     parser.add_argument("--port", default='slcan0', type=str, help="[slcan0, ...]")
     parser.add_argument("--printer", default=False, action='store_true', help="Print report")
     parser.add_argument("--onlytest", default=False, action='store_true', help="Print report")
@@ -48,11 +49,13 @@ if __name__ == "__main__":
 
     REPORT_PATH = "report.txt"
 
+    node_name = f"gnss_v{args.hw_version}"
+
     if not args.onlytest:
         if args.protocol == 'cyphal':
-            upload_firmware(args.protocol, "gnss_v3")
+            upload_firmware(args.protocol, node_name)
         elif args.protocol == 'dronecan':
-            upload_firmware(args.protocol, "gnss_v2")
+            upload_firmware(args.protocol, node_name)
 
     if args.protocol == 'dronecan':
         test_dronecan_gps_mag_baro(args.port, REPORT_PATH)
