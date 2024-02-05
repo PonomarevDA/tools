c5c7a68b6953d2abd0f594d2a021f8d6526ffc9e
PonomarevDA <ponomarevda96@gmail.com>
2024-02-06 02:46:21 +0300

    imporve rl-get-dronecan-params

--- src/raccoonlab_tools/scripts/dronecan/get_params.py
+++ src/raccoonlab_tools/scripts/dronecan/get_params.py
@@ -2,11 +2,12 @@
 # This software is distributed under the terms of the MIT License.
 # Copyright (c) 2023 Dmitry Ponomarev.
 import dronecan
-from raccoonlab_tools.dronecan.utils import ParametersInterface
+from raccoonlab_tools.dronecan.utils import ParametersInterface, NodeFinder
 
 def main():
     node = dronecan.make_node('slcan:/dev/ttyACM0', node_id=100, bitrate=1000000, baudrate=1000000)
-    params_interface = ParametersInterface(node=node, target_node_id=50)
+    target_node_id = NodeFinder(node).find_online_node()
+    params_interface = ParametersInterface(node=node, target_node_id=target_node_id)
     all_params = params_interface.get_all()
     for param in all_params:
         print(param)
