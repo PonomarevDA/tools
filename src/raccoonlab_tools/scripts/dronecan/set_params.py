682aa592baea3a6b57ec6d5e11b03ac111c01a02
PonomarevDA <ponomarevda96@gmail.com>
2024-02-06 03:04:56 +0300

    add rl-set-dronecan-params

---
+++ src/raccoonlab_tools/scripts/dronecan/set_params.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# This software is distributed under the terms of the MIT License.
+# Copyright (c) 2023 Dmitry Ponomarev.
+import yaml
+import dronecan
+import argparse
+from raccoonlab_tools.dronecan.utils import Parameter, ParametersInterface, NodeFinder
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True)
+    args = parser.parse_args()
+
+    node = dronecan.make_node('slcan:/dev/ttyACM0', node_id=100, bitrate=1000000, baudrate=1000000)
+    target_node_id = NodeFinder(node).find_online_node()
+    params_interface = ParametersInterface(node=node, target_node_id=target_node_id)
+
+    with open(args.config, "r", encoding='UTF-8') as stream:
+        params = yaml.safe_load(stream)['params']
+        for name in params:
+            desired_param = Parameter(name=name, value=params[name])
+            param_after_change = params_interface.set(desired_param)
+            print(param_after_change)
+
+if __name__ =="__main__":
+    main()
