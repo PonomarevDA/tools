d01a9161b84f14ebcc2a43e454048feaa74e6057
PonomarevDA <ponomarevda96@gmail.com>
2024-02-10 12:55:27 +0300

    move ds015 dsdl to ~/.cyphal/ds015_dsdl and remove legacy gnss scripts

--- cyphal/setup_linux_slcan.sh
+++ cyphal/setup_linux_slcan.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export CYPHAL_PATH="$HOME/.cyphal/zubax_dsdl:$HOME/.cyphal/public_regulated_data_types/"
+export CYPHAL_PATH="\
+$HOME/.cyphal/zubax_dsdl:\
+$HOME/.cyphal/public_regulated_data_types/:\
+$HOME/.cyphal/ds015_dsdl/"
+
 export UAVCAN__NODE__ID=127
 export UAVCAN__CAN__IFACE="slcan:/dev/ttyACM0@1000000"
 export UAVCAN__CAN__BITRATE="1000000 1000000"
