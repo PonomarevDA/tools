d622a3b59673596a1b15bad8d4506842f213dcf2
PonomarevDA <ponomarevda96@gmail.com>
2024-03-05 18:21:44 +0300

    update rl-test-dronecan-lights

--- src/raccoonlab_tools/scripts/dronecan/test_lights.py
+++ src/raccoonlab_tools/scripts/dronecan/test_lights.py
@@ -10,16 +10,17 @@ import pytest
 import dronecan
 from enum import IntEnum
 
-from raccoonlab_tools.dronecan.global_node import DronecanNode
 from raccoonlab_tools.common.protocol_parser import CanProtocolParser, Protocol
+from raccoonlab_tools.dronecan.global_node import DronecanNode
 from raccoonlab_tools.dronecan.utils import Parameter, ParametersInterface, NodeCommander
 
-PARAM_LIGHTS_ID                 = 'light.id'
+PARAM_LIGHTS_ID                 = 'lights.id'
 PARAM_LIGHTS_MAX_INTENSITY      = 'lights.max_intensity'
 PARAM_LIGHTS_DEFAULT_COLOR      = 'lights.default_color'
 PARAM_LIGHTS_TYPE               = 'lights.type'
 PARAM_LIGHTS_BLINK_PERIOD       = 'lights.blink_period_ms'
 PARAM_LIGHTS_DUTY_CYCLE_PCT     = 'lights.duty_cycle_pct'
+PARAM_LIGHTS_VERBOSE            = 'lights.verbose'
 
 class ParamLightsId(IntEnum):
     UI_LEDS = 0
@@ -44,9 +45,9 @@ RGB565_FROM_COLOR = [
     dronecan.uavcan.equipment.indication.RGB565(red=0, green=63, blue=0),
     dronecan.uavcan.equipment.indication.RGB565(red=0, green=0, blue=31),
     dronecan.uavcan.equipment.indication.RGB565(red=31, green=0, blue=31),
-    dronecan.uavcan.equipment.indication.RGB565(red=31, green=61, blue=0),
-    dronecan.uavcan.equipment.indication.RGB565(red=0, green=61, blue=31),
-    dronecan.uavcan.equipment.indication.RGB565(red=31, green=61, blue=31),
+    dronecan.uavcan.equipment.indication.RGB565(red=31, green=63, blue=0),
+    dronecan.uavcan.equipment.indication.RGB565(red=0, green=63, blue=31),
+    dronecan.uavcan.equipment.indication.RGB565(red=31, green=63, blue=31),
 ]
 
 
@@ -82,7 +83,8 @@ class LightsNode:
         commander.store_persistent_states()
         commander.restart()
 
-def compare_colors(first, second):
+def compare_colors(first: dronecan.uavcan.equipment.indication.LightsCommand,
+                   second: dronecan.uavcan.equipment.indication.LightsCommand) -> bool:
     return first.red == second.red and first.green == second.green and first.blue == second.blue
 
 
@@ -94,9 +96,18 @@ def test_transport():
     """
     assert CanProtocolParser.verify_protocol(white_list=[Protocol.DRONECAN])
 
+@pytest.mark.dependency()
+def test_lights_feedback():
+    lights = LightsNode()
+    config = [Parameter(name=PARAM_LIGHTS_VERBOSE, value=1)]
+    lights.configure(config)
+    recv_color = lights.recv_color()
+    assert recv_color is not None
 
-@pytest.mark.dependency(depends=["test_transport"])
+
+@pytest.mark.dependency(depends=["test_transport", "test_lights_feedback"])
 class TestSolid:
+    """Solid mode and (arm or no LightsCommand) => color must be constant"""
     color = secrets.choice(list(Colors))
     random_color_config = [
         Parameter(name=PARAM_LIGHTS_ID, value=ParamLightsId.UI_LEDS),
@@ -105,6 +116,7 @@ class TestSolid:
         Parameter(name=PARAM_LIGHTS_TYPE, value=ParamLightsType.SOLID),
         Parameter(name=PARAM_LIGHTS_BLINK_PERIOD, value=secrets.randbelow(1001)),
         Parameter(name=PARAM_LIGHTS_DUTY_CYCLE_PCT, value=secrets.randbelow(101)),
+        Parameter(name=PARAM_LIGHTS_VERBOSE, value=1),
     ]
 
     @staticmethod
@@ -115,18 +127,20 @@ class TestSolid:
         lights = LightsNode()
         lights.configure(TestSolid.random_color_config)
 
-        recv_color = lights.recv_color()
-        expected_color = RGB565_FROM_COLOR[TestSolid.color]
+        recv = lights.recv_color()
+        expected = RGB565_FROM_COLOR[TestSolid.color]
 
-        assert compare_colors(recv_color, expected_color), f"{recv_color} != {expected_color}"
+        assert compare_colors(recv, expected), (f"{TestSolid.color}: {recv} != {expected}")
 
     @staticmethod
+    @pytest.mark.skip(reason="not implemented yet")
     def test_random_color_when_armed():
         pass  # add implementation here
 
 
-@pytest.mark.dependency(depends=["test_transport"])
+@pytest.mark.dependency(depends=["test_transport", "test_lights_feedback"])
 class TestBlinking:
+    """Blink mode and (arm or no LightsCommand) => color must blink"""
     color = secrets.choice(list(Colors))
     config = [
             Parameter(name=PARAM_LIGHTS_ID, value=ParamLightsId.UI_LEDS),
@@ -134,7 +148,8 @@ class TestBlinking:
             Parameter(name=PARAM_LIGHTS_DEFAULT_COLOR, value=color),
             Parameter(name=PARAM_LIGHTS_TYPE, value=ParamLightsType.BLINKING),
             Parameter(name=PARAM_LIGHTS_BLINK_PERIOD, value=1000),
-            Parameter(name=PARAM_LIGHTS_DUTY_CYCLE_PCT, value=0),
+            Parameter(name=PARAM_LIGHTS_DUTY_CYCLE_PCT, value=secrets.randbelow(81) + 10),
+            Parameter(name=PARAM_LIGHTS_VERBOSE, value=1),
         ]
     @staticmethod
     def test_after_restart():
@@ -144,55 +159,74 @@ class TestBlinking:
         enabled_color = RGB565_FROM_COLOR[TestBlinking.color]
         disabled_color = dronecan.uavcan.equipment.indication.RGB565(red=0, green=0, blue=0)
 
-        enabled_colors_counter = 0
-        disabled_colors_counter = 0
-        unexpected_colors_counter = 0
+        enabled_counter = 0
+        disabled_counter = 0
+        unexpected_counter = 0
 
         # Skip first enabled wave
         for _ in range(100):
             color = lights.recv_color()
             if compare_colors(color, enabled_color):
-                enabled_colors_counter += 1
+                enabled_counter += 1
             elif compare_colors(color, disabled_color):
-                disabled_colors_counter += 1
+                disabled_counter += 1
             else:
-                unexpected_colors_counter += 1
-            
-        assert unexpected_colors_counter == 0, f"Got an unexpected color (need {enabled_color})"
-        assert enabled_colors_counter > 0, "Enabled color never appeared"
-        assert disabled_colors_counter > 0, "Disabled color never appeared"
+                unexpected_counter += 1
+
+        total_counter = enabled_counter + disabled_counter + unexpected_counter
+
+        hint = (
+            f"{TestBlinking.__doc__}. "
+            f"Enabled: {enabled_counter}/{total_counter}. "
+            f"Disabled: {disabled_counter}/{total_counter}. "
+            f"Unexpected: {unexpected_counter}/{total_counter}. "
+        )
+
+        assert unexpected_counter == 0, f"{hint}"
+        assert enabled_counter > 0, f"{hint}"
+        assert disabled_counter > 0, f"{hint}"
 
 
-@pytest.mark.dependency(depends=["test_transport"])
+@pytest.mark.dependency(depends=["test_transport", "test_lights_feedback"])
 class TestUiLedsMode:
+    """UI RGB LEDs mode"""
     color = secrets.choice(list(Colors))
     random_config = [
         Parameter(name=PARAM_LIGHTS_ID, value=ParamLightsId.UI_LEDS),
         Parameter(name=PARAM_LIGHTS_MAX_INTENSITY, value=100),
         Parameter(name=PARAM_LIGHTS_DEFAULT_COLOR, value=color),
-        Parameter(name=PARAM_LIGHTS_TYPE, value=ParamLightsType.SOLID),
+        Parameter(name=PARAM_LIGHTS_TYPE, value=secrets.randbelow(3)),
         Parameter(name=PARAM_LIGHTS_BLINK_PERIOD, value=secrets.randbelow(1001)),
         Parameter(name=PARAM_LIGHTS_DUTY_CYCLE_PCT, value=secrets.randbelow(101)),
+        Parameter(name=PARAM_LIGHTS_VERBOSE, value=1),
     ]
     @staticmethod
-    def test_ui_leds_mode():
-        """
-        When the autopilot is disarmed (ArmingStatus is Disarm or RawCommand is not >= 0)
-        the node is controlled by the LightCommand command.
-        """
+    def test_ui_leds_mode(length=128):
+        """Disarmed => the light is controlled by the LightCommand command"""
         lights = LightsNode()
         lights.configure(TestUiLedsMode.random_config)
 
-        for idx in range(128):
+        recv_colors = []
+        for idx in range(length):
             lights.send_single_light_command(red=idx%32, green=idx%64, blue=idx%32, id=ParamLightsId.UI_LEDS)
-            for _ in range(2):
+            lights.node.node.spin(0.05)
+            for _ in range(10):
                 color = lights.recv_color()
-            assert color.red == idx%32 and color.green == idx%64 and color.blue == idx%32
+            recv_colors.append(color)
+
+        for idx, color in enumerate(recv_colors):
+            expected = dronecan.uavcan.equipment.indication.RGB565(red=idx%32, green=idx%64, blue=idx%32)
+            assert compare_colors(color, expected), f"{TestUiLedsMode.__doc__}: {color} != {expected}"
 
 
 def main():
-    cmd = ["pytest", os.path.abspath(__file__), "--tb=no", "-v", '-W', 'ignore::DeprecationWarning']
-    cmd += sys.argv[1:]
+    cmd = ["pytest", os.path.abspath(__file__)]
+    cmd += ['--tb=no']  # No traceback at all
+    cmd += ['-v']  # Increase verbosity
+    cmd += ['-W', 'ignore::DeprecationWarning']  # Ignore specific warnings
+    cmd += sys.argv[1:] # Forward optional user flags
+    print(len(cmd))
+    print(cmd)
     sys.exit(subprocess.call(cmd))
 
 if __name__ == "__main__":
